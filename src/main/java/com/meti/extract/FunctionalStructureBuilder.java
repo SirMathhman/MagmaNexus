4027c16c0940ead56e2928a50aff10015ffb8683
SirMathhman <sirmathhman@gmail.com>
2020-04-04 14:51:26 -0400

    Remove interface usage

--- src/main/java/com/meti/extract/FunctionalStructureBuilder.java
+++ src/main/java/com/meti/extract/FunctionalStructureBuilder.java
@@ -1,9 +1,13 @@
 package com.meti.extract;
 
+import com.meti.JSONWritable;
 import com.meti.Node;
 import com.meti.Type;
+import com.meti.data.Cache;
+import com.meti.writable.Function;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class FunctionalStructureBuilder implements StructureBuilder {
@@ -25,7 +29,7 @@ public class FunctionalStructureBuilder implements StructureBuilder {
 
 	@Override
 	public Structure build() {
-		return new Structure(keys, name, type, content);
+		return new BuiltStructure(keys, name, type, content);
 	}
 
 	@Override
@@ -46,5 +50,47 @@ public class FunctionalStructureBuilder implements StructureBuilder {
 	@Override
 	public StructureBuilder withType(Type type) {
 		return new FunctionalStructureBuilder(content, keys, name, type);
+	}
+
+	private static final class BuiltStructure implements Structure {
+		private final Node content;
+		private final List<StructureKey> keys;
+		private final String name;
+		private final Type type;
+
+		private BuiltStructure(List<StructureKey> keys, String name, Type type, Node content) {
+			this.keys = Collections.unmodifiableList(keys);
+			this.name = name;
+			this.type = type;
+			this.content = content;
+		}
+
+		@Override
+		public Node appendToCache(Cache cache) {
+			cache.append(name, toWritable());
+			return new EmptyNode();
+		}
+
+		@Override
+		public boolean isValid() {
+			return keys.contains(StructureKey.VAR) || keys.contains(StructureKey.VAL);
+		}
+
+		@Override
+		public String render() {
+			return ((isMutable()) ? "var" : "val") +
+			       " " + name +
+			       " : () => " + type +
+			       " = " + content.render();
+		}
+
+		private boolean isMutable() {
+			return keys.contains(StructureKey.VAR);
+		}
+
+		@Override
+		public JSONWritable toWritable() {
+			return new Function(name, type.toWritable(), content.toWritable());
+		}
 	}
 }
